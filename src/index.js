7094f3ac465c829f95c2fd4390627ceca4a00382
Krzysiu <kheciek@gmail.com>
2017-11-08 21:38:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import Title from './components/Title.js';
+import uuid from 'uuid';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
